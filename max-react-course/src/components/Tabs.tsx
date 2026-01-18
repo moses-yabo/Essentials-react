d4ef65125d43a40a5a4e7b17fe26a39417abc4de
moses-yabo <yabo.lindile@younglings.africa>
2026-01-19 00:16:17 +0200

    WIP

--- max-react-course/src/components/Tabs.tsx
+++ max-react-course/src/components/Tabs.tsx
@@ -3,10 +3,14 @@ import type React from "react";
 type Tabs = {
   children: React.ReactElement;
   buttons: React.ReactElement;
-  ButtonContainer: React.ElementType;
+  ButtonContainer: React.ElementType | undefined;
 };
 
-export default function Tabs({ children, buttons, ButtonContainer }: Tabs) {
+export default function Tabs({
+  children,
+  buttons,
+  ButtonContainer = "menu",
+}: Tabs) {
   return (
     <>
       <ButtonContainer>{buttons}</ButtonContainer>
